610be0909cc988c84b5554c4eca9c50fd32f0a5c
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-17 00:07:09 +0800

    finished

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string GetInput(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+}
+
+string num = GetInput("Введите пятизначное число: ");
+if (num[0] == num[4] && num[1] == num[3])
+{
+    Console.WriteLine($"Число {num} является палиндромом.");
+}
+ else
+    Console.WriteLine($"Число {num} не является палиндромом.");
